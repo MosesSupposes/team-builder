4972b6aca2fe656d0ee5f161e178d33b980c1ce6
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-21 13:00:30 -0700

    add a Form component with controlled inputs

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+export default function Form(props) {
+    const [newTeamMember, setNewTeamMember] = useState({
+        name: '',
+        email: '',
+        role: ''
+    })
+
+    const handleChange = event => {
+        setNewTeamMember({ ...newTeamMember, [event.target.name]: event.target.value })
+    }
+    
+    const handleSubmit = event => {
+        event.preventDefault()
+    }
+    
+    return (
+        <>
+        <h2>Add a new Team Member</h2>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                name="name"
+                placeholder="name"
+                value={newTeamMember.name}
+                onChange={handleChange}
+            />
+            <input 
+                type="text" 
+                name="email"
+                placeholder="email"
+                value={newTeamMember.email}
+                onChange={handleChange}
+            />
+            <input 
+                type="text" 
+                name="role"
+                placeholder="role"
+                value={newTeamMember.role}
+                onChange={handleChange}
+            />
+        </form>
+        </>
+    )
+}
