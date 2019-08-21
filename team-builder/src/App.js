a05c3d1e3091489e91d47eb5948e75616b17f032
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-21 12:36:12 -0700

    set state to dummy data

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import './App.css'
+import teamData from './data'
 
 export default function App() {
+  const [team, setTeam] = useState(teamData)
+  
   return (
     <div className="App">
-      Team Builder
+      <h1>Team Builder</h1>
     </div>
   )
 }
