d83cad7a2ee38598991351afa01f02c5921ac4c3
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-21 12:37:34 -0700

    add IDs to dummy data

--- team-builder/src/data.js
+++ team-builder/src/data.js
@@ -1,20 +1,24 @@
 export default [
     {
+        id: 1,
         name: 'Kyle',
         email: 'kyle@gmail.com',
         role: 'frontend engineer'
     },
     {
+        id: 2,
         name: 'Angela',
         email: 'angela@gmail.com',
         role: 'designer'
     },
     {
+        id: 3,
         name: 'Mark',
         email: 'mark@yahoo.com',
         role: 'full-stack engineer'
     },
     {
+        id: 4,
         name: 'Steve',
         email: 'steve@hotmail.com',
         role: 'backend engineer'
