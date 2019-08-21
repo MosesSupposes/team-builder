1ceed541fbc2074cd04007f95c7d6071722c7063
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-21 12:35:57 -0700

    add dummy data

---
+++ team-builder/src/data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: 'Kyle',
+        email: 'kyle@gmail.com',
+        role: 'frontend engineer'
+    },
+    {
+        name: 'Angela',
+        email: 'angela@gmail.com',
+        role: 'designer'
+    },
+    {
+        name: 'Mark',
+        email: 'mark@yahoo.com',
+        role: 'full-stack engineer'
+    },
+    {
+        name: 'Steve',
+        email: 'steve@hotmail.com',
+        role: 'backend engineer'
+    },
+]
