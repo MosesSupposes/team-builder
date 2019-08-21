9732e86a8e296cc14232cad646c8ecea5ffeb3a1
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-21 12:44:16 -0700

    render the team data

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,12 +3,16 @@ import React, { useState } from 'react'
 import './App.css'
 import teamData from './data'
 
+import TeamMemberList from './components/TeamMemberList'
+
 export default function App() {
   const [team, setTeam] = useState(teamData)
   
   return (
     <div className="App">
       <h1>Team Builder</h1>
+
+      <TeamMemberList teamMembers={team} />
     </div>
   )
 }
